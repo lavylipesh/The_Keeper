626022f84a500afdc0aec0fad8d700d388fdc55e
lavylipesh <lilibethlangat@yahoo.com>
2019-09-03 11:09:04 -0400

    add function save user to run.py

--- run.py
+++ run.py
@@ -14,7 +14,15 @@ def keep():
     print("| |     \ \   | |___________   | |_________      | |                ")
     print("| |      \ \  | |___________|  | |_________|     |_|                ")
 keep()   
-def create_user():
+def create_user(username,password):
     '''
     function to create a new user
     '''
+    new_user = User(username,password):
+    return new_user
+def save_user(user):
+    '''
+    a function to save a user
+    '''
+    User.save_user()
+def         
