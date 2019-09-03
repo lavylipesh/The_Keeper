40871ee139324358c36ac6a86990a303f0dc6e45
lavylipesh <lilibethlangat@yahoo.com>
2019-09-03 16:38:33 -0400

    delete function to run.py

--- run.py
+++ run.py
@@ -26,3 +26,34 @@ def save_user(user):
     a function to save a user
     '''
     User.save_user()
+def  create_credentials(account,username,password):
+    '''
+    function that creates new credentials
+    '''
+    #new_credentials = Credentials(account,username,password):
+    return new_credentials
+def save_credentials(credentials):
+    '''
+    function that saves all our creaated credentials
+    '''
+    Credentials.save_credentials()
+def delete_credentials(credentials):
+    '''
+    function to delete credentials
+    '''
+    credentials.delete_credentials()
+def search_credentials(username):
+    '''
+    functio that finds credentials by username and returns the credentials
+    '''
+    return Credentials
+def test_credentials_exist(username):
+    '''
+    function that checks if credentials exist
+    '''
+    return Credentials
+def display_credentials():
+    '''
+    function that returns all saved credentials
+    '''
+    return Credentials.display_credentials()
