6e10246e803577055da37cf861326e0a4bc1519c
lavylipesh <lilibethlangat@yahoo.com>
2019-09-02 22:48:32 -0400

    create a class user that takes in two parameters

---
+++ User.py
@@ -0,0 +1,11 @@
+class User:
+    """
+    class that generates new instances of users
+    """
+    user_list = []
+def __init__(self,username,password):
+    '''
+    __init__ method that helps us define properties for our objects
+    '''
+    self.username = username
+    self.password = password    
